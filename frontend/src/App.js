2b9e9d72c6478b560bceadf5821dadf28dc51217
huongtx <phamhyta@gmail.com>
2022-10-26 22:52:18 +0700

    add product in home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import data from './data';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">Amazon</a>
       </header>
-      <main>List product</main>
+      <main>
+        <h1>Feature Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
