d1ba5b95420e342496f5f702b6808402092ac5b7
huongtx <phamhyta@gmail.com>
2022-10-26 16:43:11 +0700

    create project frontend react

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Amazon</a>
+      </header>
+      <main>List product</main>
+    </div>
+  );
+}
+
+export default App;
