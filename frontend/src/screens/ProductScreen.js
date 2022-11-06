b26ea7c8b447358ac774e056a5d53a4bfca632d2
huongtx <phamhyta@gmail.com>
2022-11-06 11:06:33 +0700

    :sunglasses: add to cart step3

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { useContext, useEffect, useReducer, useState } from 'react';
 import logger from 'use-reducer-logger';
@@ -29,6 +29,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
   const [{ loading, error, product }, dispatch] = useReducer(logger(reducer), {
@@ -65,6 +66,7 @@ function ProductScreen() {
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity },
     });
+    navigate('/cart');
   };
 
   return loading ? (
