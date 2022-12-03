d8a4abe9e5ecc9469bc3128850f6126e2549a5e0
huongtx <phamhyta@gmail.com>
2022-12-03 18:33:16 +0700

    add gg map

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -3,13 +3,14 @@ import { createContext, useReducer } from 'react';
 export const Store = createContext();
 
 const initialState = {
+  fullBox: false,
   userInfo: localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null,
   cart: {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
-      : {},
+      : { location: {} },
     paymentMethod: localStorage.getItem('paymentMethod')
       ? localStorage.getItem('paymentMethod')
       : '',
@@ -21,6 +22,11 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
+    case 'SET_FULLBOX_ON':
+      return { ...state, fullBox: true };
+    case 'SET_FULLBOX_OFF':
+      return { ...state, fullBox: false };
+
     case 'CART_ADD_ITEM':
       //add to cart
       const newItem = action.payload;
@@ -62,6 +68,17 @@ function reducer(state, action) {
         cart: {
           ...state.cart,
           shippingAddress: action.payload,
+        },
+      };
+    case 'SAVE_SHIPPING_ADDRESS_MAP_LOCATION':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            location: action.payload,
+          },
         },
       };
     case 'SAVE_PAYMENT_METHOD':
